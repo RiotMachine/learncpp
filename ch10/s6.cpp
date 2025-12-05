c6a3b8ce3ecb1d9bcfdb01893c41b197ef3de8b1
Jacob J <jirak87@outlook.com>
2025-12-05 17:14:20 -0600

    static_cast<type>(var and/or val);

---
+++ ch10/s6.cpp
@@ -0,0 +1,11 @@
+/*
+Explicit type conversion explicitly converts one data type to another
+whereas implicit type conversion does this implicitly.
+*/
+
+/* Alex's Answer
+Implicit type conversion is automatically performed whenever one data type is expected, 
+but a different data type is supplied.
+Explicit type conversion happens when the programmer uses a type cast 
+to explicitly convert a value from one type to another type.
+*/
