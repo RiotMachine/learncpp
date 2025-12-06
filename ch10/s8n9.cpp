41f070d6749b88cbde96949f20ea9cdb87c0f08b
Jacob J <jirak87@outlook.com>
2025-12-05 18:42:12 -0600

    type deduction and intro to the auto keyword

---
+++ ch10/s8n9.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <typeinfo>
+
+// auto keyword not recommended for functions 
+/// w/o a good reason
+// also does not work for function params
+/// until c++20 and then it's not type deduction
+/// but function templates
+auto sum(int x, double y)
+{
+    return x + y;
+}
+
+
+int main()
+{
+    auto x{ 3 };
+    auto y{ 5.1f };
+    auto z{ 66.0 };
+
+    auto foo{ x / y };
+    auto bar{ y * z };
+    
+    std::cout << foo << '\t' << typeid(foo).name() << '\n';
+    std::cout << bar << '\t' << typeid(bar).name() << '\n';
+
+    return 0;
+}
