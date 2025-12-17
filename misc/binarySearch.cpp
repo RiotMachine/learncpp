1889f900cb7e4ca2c99fadf1b6d8c7e0f042d88b
Jacob J <jirak87@outlook.com>
2025-12-16 18:50:12 -0600

    Lazily working thru binary sort.

---
+++ misc/binarySearch.cpp
@@ -0,0 +1,29 @@
+// how does one make a list in cpp?
+// how do you ensure that the desired object is even in the list?
+// how do you get the object once found back to main()?
+int binarySearch(list, std::string_view desiredObject)
+{
+    middleObject = 
+    if desiredObject < middleObject
+    {
+        binarySearch(lowerHalflist);
+    }
+    else if desiredObject > middleObject
+    {
+        binarySearch(higherHalflist);
+    }
+    else
+    {
+        return 
+    } 
+}
+
+int main()
+{
+    std::cout << "Which contact do you want?";
+    std::string name;
+    std::cin >> name;
+    binarySearch(list, name)
+
+    return 0;
+}
