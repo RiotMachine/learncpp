cfa4fad642df22f3ae54bc760238224ecdbbcaba
Jacob J <jirak87@outlook.com>
2026-01-08 04:57:20 -0600

    Playing around a bit more with returning by reference, and its nuances.

--- ch12/u12.cpp
+++ ch12/u12.cpp
@@ -16,10 +16,20 @@ void launchProgram(const std::string_view name)
         std::cout << "Yay!\n";
 }
 
+// 5 is in main's scope so it lives until the end of the expression in main
+// if 5 itself were returned, it would die then the returned reference would be dangling
+/// [technically, the refs are to a temp object holding the rvalue 5]
+const int& printFive(const int& five=5)
+{
+    return five;
+}
+
 int main()
 {
     std::string_view program{ getProgramName() };
     launchProgram(program);
+
+    std::cout << printFive() << '\n';
 
     return 0;
 }
