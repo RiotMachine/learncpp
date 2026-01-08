c9fc21b8ecc8c25600bc2aca8eb8ee8d116bc966
Jacob J <jirak87@outlook.com>
2026-01-08 02:35:54 -0600

    More toying with pass by address.

---
+++ ch12/u101.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+// [[maybe_unused]] gets rid of compiler warnings about ptr2 being set but not used
+void nullify([[maybe_unused]] int* ptr2)
+{
+    ++(*ptr2);
+    ptr2 = nullptr; // Make the function parameter a null pointer
+}
+
+int main()
+{
+    int x{ 5 };
+    int* ptr{ &x }; // ptr points to x
+
+    std::cout << "x is " << x << "\tptr is " << (ptr ? "non-null\n" : "null\n");
+
+    nullify(ptr);
+
+    std::cout << "x is " << x << "\tptr is " << (ptr ? "non-null\n" : "null\n");
+    return 0;
+}
