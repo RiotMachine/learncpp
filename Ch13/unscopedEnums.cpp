dcb4dc726fa801b6ad80b90d1908fdfe0da7921b
Jacob J <jirak87@outlook.com>
2026-01-16 00:48:44 -0600

    Added namespacing and aliasing to season printing.

--- Ch13/unscopedEnums.cpp
+++ Ch13/unscopedEnums.cpp
@@ -2,13 +2,7 @@
 #include <map>
 #include <string_view>
 
-enum Opponent
-{
-    colts,
-    cowboys,
-    eagles,
-    texans,
-};
+// enumerators of unscoped enumerations occupy same scope as enumeration
 
 enum Result
 {
@@ -17,39 +11,52 @@ enum Result
     tie,
 };
 
-constexpr std::string_view printOpp(Opponent opp)
+namespace NFL
 {
-    switch (opp)
+    enum Opponent
     {
-    case colts:    return "Colts";
-    case cowboys:  return "Cowboys";
-    case eagles:   return "Eagles";
-    case texans:   return "Texans";
-    default:       return "???";
-    }
-}
+        colts,
+        cowboys,
+        eagles,
+        texans,
+    };
 
-void printResult(Opponent opp, Result result)
-{
-    std::cout << "Verses the " << printOpp(opp) << '\t';
-    if (result == win)
-        std::cout << "Chiefs won!\n";
-    else if (result == loss)
-        std::cout << "Chiefs lost :(\n";
-    else
-        std::cout << "It was a tie, bro\n";
+    constexpr std::string_view printOpp(Opponent opp)
+    {
+        switch (opp)
+        {
+        case colts:    return "Colts";
+        case cowboys:  return "Cowboys";
+        case eagles:   return "Eagles";
+        case texans:   return "Texans";
+        default:       return "???";
+        }
+    }
+
+    void printResult(Opponent opp, Result result)
+    {
+        std::cout << "\tVerses the " << printOpp(opp) << '\t';
+        if (result == win)
+            std::cout << "win!\n";
+        else if (result == loss)
+            std::cout << "loss :(\n";
+        else
+            std::cout << "tie, bro\n";
+    }
 }
 
 int main()
 {
-    std::map<Opponent, Result> season;
+    using Season = std::map<NFL::Opponent, Result>;
 
-    season.insert({ texans, loss });
-    season.insert({ eagles, loss });
-    season.insert({ colts, win });
+    Season chiefs;
+    chiefs.insert({ NFL::texans, loss });
+    chiefs.insert({ NFL::eagles, loss });
+    chiefs.insert({ NFL::colts, win });
 
-    for (auto const& [key, val] : season)
-        printResult(key, val);
+    std::cout << "Chiefs season:\n";
+    for (auto const& [key, val] : chiefs)
+        NFL::printResult(key, val);
 
     return 0;
 }
