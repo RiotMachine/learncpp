4efa007ef065ab38236c9ee3d5b67b4de056b867
Jacob J <jirak87@outlook.com>
2026-01-17 05:02:45 -0600

    Tweaks

--- ch13/structsPlus.cpp
+++ ch13/structsPlus.cpp
@@ -38,7 +38,7 @@ std::ostream& operator<<(std::ostream&out, WltResult result)
     case WltResult::win:  return out << "win";
     case WltResult::loss: return out << "loss";
     case WltResult::tie:  return out << "tie";
-    default:        return out << "???";
+    default:              return out << "???";
     }
 }
 
