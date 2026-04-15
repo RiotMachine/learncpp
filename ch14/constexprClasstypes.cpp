89aad45a5c5e48c07d55d6f92c63e1527e93c2e2
Jacob J <jirak87@outlook.com>
2026-04-15 17:24:34 -0500

    Working on constexpr aggregates and classes.

---
+++ ch14/constexprClasstypes.cpp
@@ -0,0 +1,59 @@
+/*
+Constexpr aggregates and classes
+
+Notes:
+implicitly defined constructors are constexpr if possible
+explicitly defaulted constructors must be declared so
+*/
+
+#include <cmath>
+#include <iostream>
+
+struct Coordinate // Aggregate
+{
+    int m_x{ };
+    int m_y{ };
+};
+
+class Vector // Non-aggregate
+{
+private:
+    int m_x1{ };
+    int m_x2{ };
+
+public:
+    constexpr Vector(int x1, int x2)
+        : m_x1{ x1 }, m_x2{ x2 }
+    {
+    }
+
+    constexpr Vector(const Coordinate& c)
+        : Vector(c.m_x, c.m_y) {}
+
+    constexpr int x1() const { return m_x1; }
+    constexpr int x2() const { return m_x2; }
+
+    friend std::ostream& operator<<(std::ostream& out, const Vector& v)
+    {
+        return out << "(" << v.m_x1 << "," << v.m_x2 << ")";
+    }
+};
+
+constexpr double magnitude(const Vector& v)
+{
+    return std::sqrt( std::pow(v.x1(), 2) + std::pow(v.x2(), 2) );
+}
+
+int main()
+{
+    // Usually ok to make aggregates constexpr by default
+    constexpr Coordinate c{4, 5};
+    // Classes need a constexpr constructor to be literal types
+    /// Literal types := can be used as the type of a constexpr value
+    constexpr Vector v{ Coordinate{4,5} };
+    constexpr double m{ magnitude(v) };
+
+    std::cout << "Vector " << v << " has magnitude " << m << '\n';
+
+    return 0;
+}
