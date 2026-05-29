4444f78392ed16e7103ccbb96fe8490649db0281
Jacob J <jirak87@outlook.com>
2026-05-29 16:59:36 -0500

    For types that supportmove semantics, pass by const reference and return by type.

---
+++ ch16/moveSemantics.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+std::vector<int> fillVector(int x)
+{
+    std::vector<int> v(x);
+    std::cout << "Enter " << x << " integers: ";
+    for (int i{ 0 }; i < x; ++i)
+        std::cin >> v[i];
+    return v;
+}
+
+int getSum(const std::vector<int>& v)
+{
+    int sum{ 0 };
+    for (int i{ 0 }; i < v.size(); ++i)
+        sum += v[i];
+    return sum;
+}
+
+int getProduct(const std::vector<int>& v)
+{
+    int product{ v.empty() ? 0 : 1 };
+    for (int i{ 0 }; i < v.size(); ++i)
+        product *= v[i];
+    return product;
+}
+
+int main()
+{
+    std::vector v{ fillVector(3) };
+    std::cout << "The sum is: " << getSum(v) << '\n';
+    std::cout << "The product is: " << getProduct(v) << '\n';
+
+    return 0;
+}
