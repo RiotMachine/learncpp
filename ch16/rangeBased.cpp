aab6f2ed6125cbaca47f4e612f7ad8cfebb033a7
Jacob J <jirak87@outlook.com>
2026-06-02 19:18:27 -0500

    Range-based for loops.

---
+++ ch16/rangeBased.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <string_view>
+#include <vector>
+
+template <typename T>
+bool search(const std::vector<T>& arr, T desire)
+{
+    for (const auto& x : arr)
+        if (x == desire)
+            return true;
+    return false;
+}
+
+std::string getName()
+{
+    std::cout << "Enter a name: ";
+    std::string input{};
+    std::cin >> input;
+    return input;
+}
+
+
+int main()
+{
+    std::vector<std::string_view> names{
+        "Alex", "Betty", "Caroline", "Dave", "Emily", "Fred", "Greg", "Holly"
+        };
+
+    std::string desName{ getName() };
+
+    std::cout << desName << " was ";
+    std::cout << (
+        search(
+            names, static_cast<std::string_view>(desName)
+            ) ? "found.\n" : "not found.\n"
+        );
+
+    return 0;
+}
