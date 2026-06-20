703c762460719e4343cd2e9781e87a2db247411d
Jacob J <jirak87@outlook.com>
2026-06-20 15:21:51 -0500

    std::arrays of compound objects

---
+++ ch17/arrayClassType.cpp
@@ -0,0 +1,41 @@
+#include <array>
+#include <iostream>
+#include <string_view>
+
+struct Item
+{
+    std::string_view name{ };
+    int gold{ };
+};
+
+template <std::size_t N>
+void print(const std::array<Item, N>& arr)
+{
+    for (const auto& item : arr)
+        std::cout << "A " << item.name << " costs " << item.gold << " gold.\n";
+}
+
+int main()
+{
+    [[maybe_unused]] constexpr std::array items{
+        Item {"sword" , 5},
+        Item {"dagger", 3},
+        Item {"club"  , 2},
+        Item {"spear" , 7}
+    };
+
+    // std::array of compound types w/o explicit inits requires double braces
+    [[maybe_unused]]  constexpr std::array<Item, 4> items2{{
+        {"sword" , 5},
+        {"dagger", 3},
+        {"club"  , 2},
+        {"spear" , 7}
+    }};
+
+    std::cout << "CTAD:\n";
+    print(items);
+    std::cout << "\ndouble braces:\n";
+    print(items2);
+
+    return 0;
+}
