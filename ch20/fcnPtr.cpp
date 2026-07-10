02b836dee3f5f133af8f75177ae7f6dd07019d5e
Jacob J. <jirak87@outlook.com>
2026-07-10 09:37:29 -0500

    Create fcnPtr.cpp

---
+++ ch20/fcnPtr.cpp
@@ -0,0 +1,59 @@
+#include <cassert>
+#include <functional>
+#include <iostream>
+
+namespace Calculator
+{
+    int add(int x, int y)      { return x + y; }
+    int subtract(int x, int y) { return x - y; }
+    int multiply(int x, int y) { return x * y; }
+    int divide(int x, int y)   { return x / y; }
+}
+
+
+using ArithmeticFcn = std::function<int(int, int)>;
+
+int getInt()
+{
+    int x;
+    std::cin >> x;
+    return x;
+}
+
+char getChar()
+{
+    char c;
+    std::cin >> c;
+    return c;
+}
+
+ArithmeticFcn getArithmeticFcn(char c)
+{
+    switch (c)
+    {
+    case '+': return Calculator::add;
+    case '-': return Calculator::subtract;
+    case '*': return Calculator::multiply;
+    case '/': return Calculator::divide;
+    default:
+        std::cerr << "getFcn(): Invalid choice.\n";
+        assert(false && "getFcn(): Invalid choice.");
+        return nullptr;
+    }
+}
+
+int main()
+{
+    std::cout << "Input an integer: ";
+    int x{ getInt() };
+    std::cout << "Input another integer: ";
+    int y{ getInt() };
+    std::cout << "Input an operator (+, -, *, /): ";
+    char c{ getChar() };
+
+    ArithmeticFcn fcnPtr{ getArithmeticFcn(c) };
+    if (fcnPtr)
+        std::cout << x << ' ' << c << ' ' << y << " = " << fcnPtr(x, y);
+    
+    return 0;
+}
