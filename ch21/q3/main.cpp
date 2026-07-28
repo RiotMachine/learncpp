1ae245a8ef90aae8cceb9b654979a15e58429d2a
Jacob J. <jirak87@outlook.com>
2026-07-28 09:30:12 -0500

    Create main.cpp

---
+++ ch21/q3/main.cpp
@@ -0,0 +1,34 @@
+#include "IntArray.h"
+#include <iostream>
+
+IntArray fillArray()
+{
+    IntArray a(5);
+
+    a[0] = 5;
+    a[1] = 8;
+    a[2] = 2;
+    a[3] = 3;
+    a[4] = 6;
+
+    return a;
+}
+
+int main()
+{
+    IntArray a{ fillArray() };
+
+    std::cout << a << '\n';
+
+    auto& ref{ a };
+    a = ref;
+
+    IntArray b(1);
+    b = a;
+
+    a[4] = 7;
+
+    std::cout << b << '\n';
+
+    return 0;
+}
