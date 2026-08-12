6ae883fd43c3d59cd6d79be09a4352111f261497
Jacob J <jirak87@outlook.com>
2026-08-12 14:58:09 -0500

    23.7 quiz

--- ch23/main.cpp
+++ ch23/main.cpp
@@ -1,14 +1,22 @@
 #include "MyVector.h"
+#include <cstddef>
 #include <iostream>
+
+template <typename T>
+void print(MyVector<T> v)
+{
+    for (std::size_t i{ }; i < v.size(); ++i)
+        std::cout << v[i] << ' ';
+    std::cout << '\n';
+}
 
 int main()
 {
+    // 23.6
     MyVector<int> vector(10);
 
     for (std::size_t i{ }; i < 10; ++i)
         vector[i] = static_cast<int>(i+1);
-
-    vector = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
     vector.resize(8);
     vector.insert(20, 5);
@@ -23,9 +31,16 @@ int main()
         vector = vector;
     }
 
-    for (std::size_t i{ }; i < vector.size(); ++i)
-        std::cout << vector[i] << ' ';
-    std::cout << '\n';
+    print(vector);
+
+
+    // 23.7
+    MyVector array{ 5, 4, 3, 2, 1 };
+    print(array);
+
+    array = { 1, 3, 5, 7, 9, 11 };
+    print(array);
+
 
     return 0;
 }
