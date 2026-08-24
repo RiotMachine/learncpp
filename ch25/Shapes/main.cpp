28100fcda43132b8130d6dcfee7df9d23c0bef19
Jacob J <jirak87@outlook.com>
2026-08-24 16:29:29 -0500

    Wrote unique_ptr logic.

--- ch25/Shapes/main.cpp
+++ ch25/Shapes/main.cpp
@@ -1,17 +1,48 @@
-#include "Circle.h"
 #include "Shape.h"
+#include "Circle.h"
 #include "Triangle.h"
 #include <algorithm>
 #include <iostream>
+#include <memory>
 #include <vector>
+
+template <typename T>
+void printArr(const std::vector<T*>& arr)
+{
+    for (const auto* ptr : arr)
+    {
+        if (ptr)
+            std::cout << *ptr << '\n';
+    }
+}
+
+template <typename T>
+void printArr(const std::vector<std::unique_ptr<T>>& arr)
+{
+    for (const auto& ptr : arr)
+    {
+        if (ptr)
+            std::cout << *ptr << '\n';
+    }
+}
 
 int getLargestRadius(const std::vector<Shape*>& arr)
 {
     int maxRadius{ };
     for (const auto* shapePtr : arr)
     {
-        auto circlePtr{ dynamic_cast<const Circle*>(shapePtr) };
-        if (circlePtr)
+        if (auto circlePtr{ dynamic_cast<const Circle*>(shapePtr) })
+            maxRadius = std::max(maxRadius, circlePtr->radius());
+    }
+    return maxRadius;
+}
+
+int getLargestRadius(const std::vector<std::unique_ptr<Shape>>& arr)
+{
+    int maxRadius{ };
+    for (const auto& shapePtr : arr)
+    {
+        if (auto circlePtr{ dynamic_cast<const Circle*>(shapePtr.get()) })
             maxRadius = std::max(maxRadius, circlePtr->radius());
     }
     return maxRadius;
@@ -19,18 +50,29 @@ int getLargestRadius(const std::vector<Shape*>& arr)
 
 int main()
 {
-    std::vector<Shape*> v{
+    std::vector<Shape*> arrRaw{
         new Circle   { {1,2}, 7 },
         new Triangle { {1,2}, {3,4}, {5,6} },
         new Circle   { {7,8}, 3 }
     };
 
-    for (const auto* shapePtr : v)
-        std::cout << *shapePtr << '\n';
+    std::vector<std::unique_ptr<Shape>> arrUnique(3);
 
-    std::cout << "The largest radius is: " << getLargestRadius(v) << '\n';
+    arrUnique[0] = std::make_unique<Circle>(
+        Point2D {1,2}, 7
+    );
+    arrUnique[1] = std::make_unique<Triangle>(
+        Point2D {1,2}, Point2D {3,4}, Point2D {5,6}
+    );
+    arrUnique[2] = std::make_unique<Circle>(
+        Point2D {7,8}, 3
+    );
 
-    for (const auto* shapePtr : v)
+    printArr(arrUnique);
+
+    std::cout << "The largest radius is: " << getLargestRadius(arrUnique) << '\n';
+
+    for (const auto* shapePtr : arrRaw)
         delete shapePtr;
 
     return 0;
