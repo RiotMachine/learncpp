7c3da6bab5815e7bcd3a0489ddee9fbe676e945a
Jacob J <jirak87@outlook.com>
2026-09-11 08:49:20 -0500

    Added random file I/O

--- ch28/fileIO.cpp
+++ ch28/fileIO.cpp
@@ -1,39 +1,62 @@
 #include <chrono>
 #include <ctime>
 #include <fstream>
+#include <iomanip>
 #include <iostream>
 #include <string>
 
 using Time = std::chrono::system_clock;
 
-int main()
+/*
+    std::ios::in  - read mode
+    std::ios::out - write mode
+    std::ios::app - append mode
+
+    [funct]g - read/input   (get)
+    [funct]p - write/output (put)
+
+    *creating a file while in read mode may cause fstream to fail
+    *cannot switch between read/write w/o a move
+*/
+
+std::time_t getTime()
 {
-    // if std::ios::in is used, creating a file may cause fstream to fail
-    std::ofstream ofile{ "test.txt", std::ios::app };
-    if (!ofile)
-    {
-        std::cerr << "File could not be opened.\n";
-        return 1;
-    }
+    // https://stackoverflow.com/a/27856440
+    return Time::to_time_t(Time::now());
+}
+
+void appendRecord(std::fstream& file)
+{
     std::cout << "Input your name: ";
     std::string name{ };
     std::getline(std::cin >> std::ws, name);
 
-    // https://stackoverflow.com/a/27856440
-    std::time_t curr_time{ Time::to_time_t(Time::now()) };
-    ofile << name << " was here at " << std::ctime(&curr_time);
+    std::time_t curr_time{ getTime() };
+    // std::setw only applies to the next extraction
+    file << std::setw(15) << std::left << name << std::right
+         << " was here at " << std::ctime(&curr_time);
+}
 
-    ofile.close();
+void read(std::fstream& file)
+{
+    std::string extract{ };
+    while (std::getline(file, extract))
+        std::cout << extract << '\n';
+}
 
-    std::ifstream ifile{ "test.txt" };
-    if (!ifile)
+int main()
+{
+    std::fstream file{ "test.txt", std::ios::app | std::ios::in };
+    if (!file)
     {
         std::cerr << "File could not be opened.\n";
         return 1;
     }
-    std::string extract{ };
-    while (std::getline(ifile, extract))
-        std::cout << extract << '\n';
+
+    appendRecord(file);
+    std::cout << '\n';
+    file.seekg(0);
+    read(file);
 
     return 0;
 }
